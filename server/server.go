d32b037c21d82e79a93751de47d52ac99adb16b3
gopbft <2689411789@qq.com>
2020-11-15 17:01:17 +0800

    test server->server

--- server/server.go
+++ server/server.go
@@ -528,7 +528,7 @@ func (serv *Server) handleTransaction(request []byte) {
 	if tx.Verify() {
 		serv.msgbuff.Msgbuffmu.Lock()
 		serv.msgbuff.TxPool[tx.GetHash()] = tx
-		if len(serv.msgbuff.TxPool)%10==0 {
+		if len(serv.msgbuff.TxPool)%1000==0 {
 			fmt.Println("server", serv.id, "has",len(serv.msgbuff.TxPool), "txs")
 		}
 		serv.msgbuff.Msgbuffmu.Unlock()
