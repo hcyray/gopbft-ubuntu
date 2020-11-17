b40d6b22e53f00a99f73d1aba7bed29e77b2bcc4
gopbft <2689411789@qq.com>
2020-11-17 15:53:39 +0800

    test server->server without clients, large timeout value

--- main.go
+++ main.go
@@ -134,7 +134,7 @@ func main() {
 		}
 	} else {
 		//invoke cliients
-		for i:=0; i<40; i++ {
+		for i:=0; i<100; i++ {
 			privatekey := datastruc.DecodePrivate(ck.Clienprivks[i])
 			theclient := client.CreateClient(i, totalserver*2, privatekey, allips[0:totalserver])
 			val := rand.Intn(400)
