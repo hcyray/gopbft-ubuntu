5cd0440a14f28dea7bbd578540bf3e899a6510ba
gopbft <2689411789@qq.com>
2020-11-18 21:16:35 +0800

    test mechanism 2

--- client/client.go
+++ client/client.go
@@ -128,14 +128,14 @@ func (client *Client) Run() {
 
 	rand.Seed(time.Now().UTC().UnixNano()+int64(client.id))
 	//var hval [32]byte
-	for i:=0; i<1000; i++ {
+	for i:=0; i<10000; i++ {
 		rannum := rand.Uint64()
 		ok, newtx := datastruc.MintNewTransaction(rannum, client.nodePubkeystr, client.nodePrvKey)
 		if ok {
 			client.BroadcastMintedTransaction(newtx, client.id, client.miners)
 		}
 		//val := rand.Intn(150)
-		val := 10
+		val := 1
 		time.Sleep(time.Millisecond*time.Duration(val))
 	}
 	fmt.Println("client", client.id, "stops")
