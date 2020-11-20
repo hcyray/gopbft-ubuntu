ef1c97a22b82c3b885a885569350356598b4c098
gopbft <2689411789@qq.com>
2020-11-20 16:45:17 +0800

    test mechanism 2

--- client/client.go
+++ client/client.go
@@ -141,7 +141,7 @@ func (client *Client) Run() {
 			}
 		}
 		//val := rand.Intn(2) + 1
-		val := 1000
+		val := 1500
 		time.Sleep(time.Nanosecond*time.Duration(val))
 	}
 	fmt.Println("client", client.id, "stops")
