b282248f5af14a81d63188b163aa1315ff46574e
gopbft <2689411789@qq.com>
2020-11-20 15:27:08 +0800

    test mechanism 2

--- pbft/pbft.go
+++ pbft/pbft.go
@@ -237,7 +237,7 @@ func (pbft *PBFT) InitialSetup() {
 		tmppubk = append(tmppubk, p.Member.PubKey)
 		p = p.Next
 	}
-	fmt.Println("instace", pbft.Id, "thinks the leader succession line is", tmpid, "pubkey not show")
+	fmt.Println("instace", pbft.Id, "thinks the leader succession line is", tmpid, "pubkey:", tmppubk)
 	fmt.Println("instance", pbft.Id, "pubkey string is", pbft.PubKeystr)
 
 	// construct genesis block
