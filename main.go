5db39874a11500ac13a16a5cf08fd4e50bdd79a3
gopbft <2689411789@qq.com>
2020-11-23 19:20:07 +0800

    debug mechanism 2

--- main.go
+++ main.go
@@ -77,7 +77,7 @@ func ReadClientKeys(fn string)  client.ClienKeys {
 }
 
 func main() {
-	fmt.Println("compact tx, backup this version!!")
+	fmt.Println("debug mechanism 2...")
 	fmt.Println("Get the cluster IPs from", os.Args[1])
 	fmt.Println("Get client keys from", os.Args[2])
 	localip := GetOutboundIP().String()
