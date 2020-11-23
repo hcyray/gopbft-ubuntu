cf1c2f14fc508c63c49776ed450f591d3f8fb122
gopbft <2689411789@qq.com>
2020-11-23 21:45:03 +0800

    debug unvalid leave-tx

--- main.go
+++ main.go
@@ -110,7 +110,7 @@ func main() {
 
 	clientnumber := 10
 	instanceoneachserver := 1
-	initialserver := 4
+	initialserver := 3
 	lateserver := 0 // 机制1测试
 	totalserver := initialserver + lateserver
 	// read client pubkeys
