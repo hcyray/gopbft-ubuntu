dd1dd06ba7a76315e239ee881a67eb95fc42bebf
gopbft <2689411789@qq.com>
2020-12-24 16:28:53 +0800

    fix propose test for new, backup

--- pbft/pbft.go
+++ pbft/pbft.go
@@ -356,7 +356,7 @@ func (pbft *PBFT) Run() {
 
 	pbft.MsgBuff.ClearTXPool()
 	for {
-		if pbft.currentHeight > 120 {
+		if pbft.currentHeight > 240 {
 			pbft.Stop()
 		}
 		if pbft.isleaving && !pbft.sentleavingtx && pbft.currentHeight>=302 {
