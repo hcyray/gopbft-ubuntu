290be378584010b7b4036048c3a74a83254f10a5
gopbft <2689411789@qq.com>
2020-12-27 19:57:22 +0800

    fix propose test for new, backup

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func main() {
 		clientnumber := 32
 		instanceoneachserver := 1
 		initialserver := 9
-		lateserver := 0 // 机制1测试
+		lateserver := 1 // 机制1测试
 		totalserver := initialserver + lateserver
 		// read client pubkeys
 		ck := ReadClientKeys(os.Args[2])
