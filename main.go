fcf128ab7e5b336dbb919b005b14c0367ae38c19
gopbft <2689411789@qq.com>
2020-12-29 15:05:37 +0800

    fix cde module, backup

--- main.go
+++ main.go
@@ -144,7 +144,7 @@ func main() {
 				fmt.Println("the ", i, "client starts")
 			}
 		}
-		time.Sleep(time.Second * 120)
+		time.Sleep(time.Second * 130)
 	} else {
 		fmt.Printf("tmp is ", tmp)
 	}
