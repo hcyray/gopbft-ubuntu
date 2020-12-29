01c3c35d35fef60d9623e9b14ea5a1dbecd880f6
gopbft <2689411789@qq.com>
2020-12-29 11:26:13 +0800

    fix cde module, backup

--- main.go
+++ main.go
@@ -114,8 +114,8 @@ func main() {
 
 		clientnumber := 32
 		instanceoneachserver := 1
-		initialserver := 4
-		lateserver := 1 // mechanism1测试
+		initialserver := 9
+		lateserver := 0 // mechanism1测试
 		totalserver := initialserver + lateserver
 		// read client pubkeys
 		ck := ReadClientKeys(os.Args[2])
@@ -143,7 +143,7 @@ func main() {
 				fmt.Println("the ", i, "client starts")
 			}
 		}
-		time.Sleep(time.Second * 60)
+		time.Sleep(time.Second * 120)
 	} else {
 		fmt.Printf("tmp is ", tmp)
 	}
--- pbft/pbft.go
+++ pbft/pbft.go
@@ -363,7 +363,7 @@ func (pbft *PBFT) Run() {
 		//if elap>74 {
 		//	pbft.Stop()
 		//}
-		if pbft.currentHeight > 80 {
+		if pbft.currentHeight > 160 {
 			pbft.Stop()
 		}
 		if pbft.isleaving && !pbft.sentleavingtx && pbft.currentHeight>=32 && false {
