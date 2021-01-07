d3c2f62241e2f6977470312f89de9e594429d678
gopbft <2689411789@qq.com>
2021-01-07 20:23:42 +0800

    change checkpoint, debug

--- pbft/pbft.go
+++ pbft/pbft.go
@@ -369,7 +369,7 @@ func (pbft *PBFT) Run() {
 		//if elap>74 {
 		//	pbft.Stop()
 		//}
-		if pbft.currentHeight > 150 {
+		if pbft.currentHeight > 180 {
 			pbft.Stop()
 		}
 		if pbft.isleaving && !pbft.sentleavingtx && pbft.currentHeight>=32 && false {
