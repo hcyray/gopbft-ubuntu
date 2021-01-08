d3843d3367b129623601602556e247d4aebab9bb
gopbft <2689411789@qq.com>
2021-01-08 19:58:26 +0800

    change checkpoint, debug

--- datastruc/CDE_algorithm.go
+++ datastruc/CDE_algorithm.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-const MAXWAITTIME = 1000 // maximum waiting time for reponse message(ms)
+const MAXWAITTIME = 2000 // maximum waiting time for reponse message(ms)
 
 type CDEdata struct {
 	mu sync.Mutex
