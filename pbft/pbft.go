177c2ab1a452a24c1f5def65c0dce7955d50961a
gopbft <2689411789@qq.com>
2021-01-08 22:01:06 +0800

    change checkpoint, debug

--- pbft/pbft.go
+++ pbft/pbft.go
@@ -1693,9 +1693,9 @@ func (pbft *PBFT) Stop() {
 		pbft.predictedconsensustimelog[h] = 2000
 	}
 	pt := make(map[int]int)
-	//for l:=0; l<pbft.succLine.Leng; l++ {
-	//	pt[l] = pbft.cdedata.CalculateConsensusDelay(l, pbft.succLine.Leng, pbft.quorumsize)[pbft.Id]/LeaderLease
-	//}
+	for l:=0; l<pbft.succLine.Leng; l++ {
+		pt[l] = pbft.cdedata.CalculateConsensusDelay(l, pbft.succLine.Leng, pbft.quorumsize)[pbft.Id]/LeaderLease
+	}
 	//for h:=100; h<=len(pbft.leaderlog); h++ {
 	//	pconsensusdelay := pt[pbft.leaderlog[h]]
 	//	pbft.predictedconsensustimelog[h] = pconsensusdelay // turnoff this when testing mechanism2
