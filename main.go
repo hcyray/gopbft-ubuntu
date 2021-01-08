2b39536183c0c8e1a0258b806ad7c848f89b8ec7
gopbft <2689411789@qq.com>
2021-01-08 15:20:58 +0800

    change checkpoint, debug

--- main.go
+++ main.go
@@ -144,7 +144,7 @@ func main() {
 				fmt.Println("the ", i, "client starts")
 			}
 		}
-		time.Sleep(time.Second * 125)
+		time.Sleep(time.Second * 175)
 	} else {
 		fmt.Printf("tmp is ", tmp)
 	}
