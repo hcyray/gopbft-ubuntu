d84ad37c5a1eb0f1cdc6cafd82f5dab697e9b88a
gopbft <2689411789@qq.com>
2021-01-08 14:46:58 +0800

    change checkpoint, debug

--- main.go
+++ main.go
@@ -131,7 +131,7 @@ func main() {
 			for i:=0; i<instanceoneachserver; i++ {
 				instanceid := i+instanceoneachserver*localid
 				theserver := server.CreateLateServer(instanceid, localip, ck.Clientpubkstrs, allips[0:initialserver], instanceoneachserver)
-				starttime := 90+10*(localid-initialserver)
+				starttime := 50+10*(localid-initialserver)
 				go theserver.LateStart(ck.Clientpubkstrs, starttime) // new nodes join serially
 				fmt.Println("server", instanceid, "starts, it is a late server, will start at", starttime, "s")
 			}
@@ -144,7 +144,7 @@ func main() {
 				fmt.Println("the ", i, "client starts")
 			}
 		}
-		time.Sleep(time.Second * 175)
+		time.Sleep(time.Second * 95)
 	} else {
 		fmt.Printf("tmp is ", tmp)
 	}
